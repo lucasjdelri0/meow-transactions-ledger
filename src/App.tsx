92b7c35e76c613659364f6e32e63e78261f6335d
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-10-21 20:13:13 +0200

    Decouple homepage from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
-import { Typography } from 'antd'
-import Primary from './layout/Primary'
-import Page from './components/Page'
+import Home from 'pages/home'
 import './App.css'
 
-const { Title, Paragraph } = Typography
-const App = (): JSX.Element => (
-  <Primary>
-    <Page>
-      <Title level={2}>Meow Challenge</Title>
-      <Paragraph>
-        This solution is intended to solve the frontend challenge proposed by
-        Meow.
-      </Paragraph>
-      <Paragraph>
-        You will be able to keep track of your financial categories, check their
-        balances, and even transfer money between them.
-      </Paragraph>
-      <div>
-        <img alt='meow' src='images/meow.png' style={{ width: 300 }} />
-      </div>
-    </Page>
-  </Primary>
-)
+const App = (): JSX.Element => <Home />
 
 export default App
